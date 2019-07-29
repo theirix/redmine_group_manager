3fdbeec4469af09874682219656a979d651fda98
theirix <theirix@gmail.com>
2019-07-29 23:12:58 +0300

    Specify exact Rails version in migration

--- db/migrate/001_create_gmanagers.rb
+++ db/migrate/001_create_gmanagers.rb
@@ -1,10 +1,10 @@
-class CreateGmanagers < ActiveRecord::Migration
+class CreateGmanagers < (Rails.version < '5.1') ? ActiveRecord::Migration : ActiveRecord::Migration[4.2]
   def change
     create_table :gmanagers do |t|
       t.integer :id_group
       t.integer :id_owner
       t.string :perm
-      
+
     end
   end
 end
